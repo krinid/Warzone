469790e17fd84cadadfda958d17dc4a2f5749f51
krinid <krinid@gmail.com>
2025-02-24 20:55:45 -0500

    update

--- Late Airlifts v2/Client_GameOrderCreated.lua
+++ Late Airlifts v2/Client_GameOrderCreated.lua
@@ -1,6 +1,6 @@
 function Client_GameOrderCreated (game, gameOrder, skip)
 	--check if an Airlift was entered, if so popup an alert to let player know it will occur at the end of a turn and not the beginning
 	if (gameOrder.proxyType=='GameOrderPlayCardAirlift') then
-		UI.Alert ("[LATE AIRLIFTS]\n\nAirlifts occur at the END OF A TURN (not the beginning) in this game.\n\nIf you don't own the territory at that time, the airlift will not occur.");
+		UI.Alert ("[LATE AIRLIFTS]\nAirlifts occur at the END OF A TURN (not the beginning) in this game.\n\nIf you no longer own the territory at that time, the airlift will not occur.");
 	end
 end
