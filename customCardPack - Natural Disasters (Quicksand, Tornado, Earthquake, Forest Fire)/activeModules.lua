68e3ad87bf56df73934b622c437b9f5986f69e24
krinid <krinid@gmail.com>
2025-02-27 01:31:50 -0500

    split CCPA into 4 submods

---
+++ customCardPack - Natural Disasters (Quicksand, Tornado, Earthquake, Forest Fire)/activeModules.lua
@@ -0,0 +1,13 @@
+--use 1 of following combos for each mod:
+--activeModules = {["Nuke"]=true, ["Pestilence"]=true, ["Isolation"]=true};
+    --future: activeModules = {["Nuke"]=true, ["Pestilence"]=true, ["Isolation"]=true, ["Airstrike"]=true};
+--activeModules = {["Shield"]=true, ["Monolith"]=true, ["Neutralize"]=true, ["Deneutralize"]=true};
+--activeModules = {["Card Block"]=true, ["Card Piece"]=true};
+    --future: activeModules = {["Card Block"]=true, ["Card Piece"]=true, ["Card Hold"]=true};
+activeModules = {["Quicksand"]=true, ["Tornado"]=true, ["Earthquake"]=true, ["Forest Fire"]=true};
+
+--Rough mod plan:
+-- mod 1: Nuke, Pestilence, Isolation, Airstrike
+-- mod 2: Shield, Monolith, Neutralize, Deneutralize, ?Portal
+-- mod 3: Card Block, Card Piece, Card Hold (future)
+-- mod 4: Quicksand, Tornado, Earthquake, Forest Fire, ?
---
+++ customCardPackage_A_refresh - Copy (6)/Server_GameCustomMessage.lua
@@ -0,0 +1,9 @@
+require("utilities");
+
+function Server_GameCustomMessage(game,playerID,payload,setReturn)
+	if (payload == "") then
+		--stuff
+	end
+
+	initialize_CardData (game);
+end
