3b9bfd5deb8360601175397f4a3a46af26be28bc
krinid <krinid@gmail.com>
2025-03-24 22:21:17 -0400

    BC2 update complete, publish final

--- BuyCards_v2/Client_GameRefresh.lua
+++ BuyCards_v2/Client_GameRefresh.lua
@@ -6,7 +6,7 @@ function Client_GameRefresh(clientGame)
     if (boolHostToSetPrices_AntiNag == nil) then boolHostToSetPrices_AntiNag = false; end --when set to true, host has already been nagged about setting prices this session, so don't nag again
 
     --if not Commerce game, do nothing; if not host, do nothing; if Commercer game & local player is host & prices have not been set, send alert to advise player to set card prices, but only if they haven't been nagged already
-    if (--[[game.Settings.CommerceGame == true and]] localPlayerIsHost==true and Mod.PublicGameData.CardData.HostHasAdjustedPricing==false and boolHostToSetPrices_AntiNag == false) then
+    if (clientGame.Settings.CommerceGame == true and localPlayerIsHost==true and Mod.PublicGameData.CardData.HostHasAdjustedPricing==false and boolHostToSetPrices_AntiNag == false) then
         UI.Alert ("You are the game host.\n\nPlease go to Game/Mod: Buy Cards v2, set the card prices, then click 'Update Prices'.");
         boolHostToSetPrices_AntiNag = true;
     end
