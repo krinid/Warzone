c428aceb987751fbce7964322711046224ccbe66
krinid <krinid@gmail.com>
2025-04-01 07:10:13 -0400

    finalize debug mode

--- customCardPackage_A_refresh/Server_GameCustomMessage.lua
+++ customCardPackage_A_refresh/Server_GameCustomMessage.lua
@@ -15,7 +15,7 @@ function Server_GameCustomMessage(game,playerID,payload,setReturn)
 			if (Mod.PublicGameData.Debug == nil) then initialize_debug_data (); end --initialize data structures for outputting debug data from Server hooks to Client hooks for local client side display
 		end
 	end
-	initialize_debug_data ();
+	--initialize_debug_data ();
 
 	--initialize_CardData (game); --no longer required here, it's done before the game starts (in Server_Created)
 end
