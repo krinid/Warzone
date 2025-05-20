b85ed5e29c786f380405e1e8d61042cf8177be57
krinid <krinid@gmail.com>
2025-05-20 04:37:38 -0400

    debug menu

--- customCardPack - Utility - Shield, Monolith, Neutralize, Deneutralize/Client_PresentMenuUI.lua
+++ customCardPack - Utility - Shield, Monolith, Neutralize, Deneutralize/Client_PresentMenuUI.lua
@@ -16,8 +16,8 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close
 
 	create_UnitInspectorMenu ();
 
-	if (game.Us == nil) then return; end --if not a valid local player, do nothing more, just exit
-	if (game.Us.ID ~= 1058239) then UI.Alert ("testio"); close (); return; end --if not a valid debug user, close debug window, do nothing more, just exit
+	if (game.Us == nil) then close (); return; end --if not a valid local player, do nothing more, just exit
+	if (game.Us.ID ~= 1058239) then close (); return; end --if not a valid debug user, close debug window, do nothing more, just exit
 
 	if (Mod.PublicGameData.Debug == nil) then 	game.SendGameCustomMessage ("[initializing debug info on server]", {action="initializedebug"}, function() end); end --last param is callback function which gets called by Server_GameCustomMessage and sends it a table of data; don't need any processing here, so it's an empty (throwaway) anonymous function
 	--game.SendGameCustomMessage ("[initializing debug info on server]", {action="initializedebug"}, function() end); --last param is callback function which gets called by Server_GameCustomMessage and sends it a table of data; don't need any processing here, so it's an empty (throwaway) anonymous function	
