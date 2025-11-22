fbec7d76c3deea45ae43761a3dc35fb9b71e37af
krinid <krinid@gmail.com>
2025-11-22 15:08:08 -0500

    bugfix Pun/Rew during manual Distro

--- Punishment and Reward/Client_GameCommit.lua
+++ Punishment and Reward/Client_GameCommit.lua
@@ -2,6 +2,10 @@ function Client_GameCommit (game, skipCommit)
 	-- skipCommit ();
 	local boolSameOrderExistsAlready = false; --indicates whether an order for A->B already exists in the order list; if so, assume it's legit and skip this order
 
+	--if game state isn't "Playing" then skip this; it is likely 'DistributingTerritories', in which case trying to a custom order will fail b/c only Pick orders are accepted here
+	if (game.Game.State ~= WL.GameState.Playing) then return; end
+	-- UI.Alert (game.Game.State .."/".. tostring (WL.GameState.ToString (game.Game.State)));
+
 	--check to see if the order exists already; only add it if it doesn't already exist; this can happen if a user commits, uncommits, recommits, etc
 	for k,existingGameOrder in pairs (game.Orders) do
 		-- print (k..", "..existingGameOrder.proxyType);
@@ -11,6 +15,7 @@ function Client_GameCommit (game, skipCommit)
 	--only do this is an order for territory A->B doesn't exist yet; if it does, it'll throw an error on user client; each of the 4 Card Pack mods will try to recreate the order w/o Immovable Specials
 	--leverage 'boolSameOrderExistsAlready' to ensure that only the 1st mod actually inserts the corrected order 
 	if (boolSameOrderExistsAlready == false) then
+		-- UI.Alert ("game state: " ..game.Game.State.."/".. tostring (WL.GameState.ToString (game.Game.State)));
 		local newOrder = WL.GameOrderCustom.Create (game.Us.ID, "[Card state snapshot]", "PunishReward|Capture card state", {}, WL.TurnPhase.ReceiveCards);
 		--b/c this function has no addOrder callback parameter, need to manually add the order into the clientgame parameter 'game'
 		local orders = game.Orders;
