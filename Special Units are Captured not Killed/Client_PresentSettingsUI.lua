a328c28db8c114186d4391f9d9c020ee7cb5cb83
krinid <krinid@gmail.com>
2026-01-31 16:21:07 -0500

    desc changes

---
+++ Special Units are Captured not Killed/Client_PresentSettingsUI.lua
@@ -0,0 +1,3 @@
+function Client_PresentSettingsUI(rootParent)
+	UI.CreateLabel (rootParent).SetText ("• When Special Units are killed, instead of dying, they become captured by the attacking player.\n• Ownership changes to the attacking player with the properties the Special Units had at the time the attack started.");
+end
