4670374c5c8e010fe8bb30e4544bbb4c4d726bad
krinid <krinid@gmail.com>
2026-03-04 04:53:30 -0500

    Portals v2 add client side custom order in ReceiveGold phase to trigger portal swaps

---
+++ Portals v2/Client_GameCommit.lua
@@ -0,0 +1,25 @@
+function Client_GameCommit (game, skipCommit)
+	-- skipCommit ();
+	local boolSameOrderExistsAlready = false; --indicates whether an order for A->B already exists in the order list; if so, assume it's legit and skip this order
+
+	--if game state isn't "Playing" then skip this; it is likely 'DistributingTerritories', in which case trying to a custom order will fail b/c only Pick orders are accepted here
+	if (game.Game.State ~= WL.GameState.Playing) then return; end
+
+	--check to see if the order exists already; only add it if it doesn't already exist; this can happen if a user commits, uncommits, recommits, etc
+	for k,existingGameOrder in pairs (game.Orders) do
+		-- print (k..", "..existingGameOrder.proxyType);
+		if (existingGameOrder.proxyType == "GameOrderCustom" and existingGameOrder.Payload == "Portals|Portal swap") then boolSameOrderExistsAlready = true; end
+	end
+
+	--only do this is an order for territory A->B doesn't exist yet; if it does, it'll throw an error on user client; each of the 4 Card Pack mods will try to recreate the order w/o Immovable Specials
+	--leverage 'boolSameOrderExistsAlready' to ensure that only the 1st mod actually inserts the corrected order 
+	if (boolSameOrderExistsAlready == false) then
+		-- UI.Alert ("game state: " ..game.Game.State.."/".. tostring (WL.GameState.ToString (game.Game.State)));
+		local newOrder = WL.GameOrderCustom.Create (game.Us.ID, "[End of orders]", "Portals|Portal swap", {}, WL.TurnPhase.ReceiveGold);
+		-- local newOrder = WL.GameOrderCustom.Create (game.Us.ID, "[End of orders]", "Portals|Portal swap", {}, WL.TurnPhase.ReceiveCards);
+		--b/c this function has no addOrder callback parameter, need to manually add the order into the clientgame parameter 'game'
+		local orders = game.Orders;
+		table.insert(orders, newOrder);
+		game.Orders = orders;
+	end
+end
--- Portals v2/Server_AdvanceTurn.lua
+++ Portals v2/Server_AdvanceTurn.lua
@@ -1,5 +1,10 @@
 function Server_AdvanceTurn_End (game, addNewOrder)
-	execute_Portal_Swaps (game, addNewOrder);
+	if (boolPortalSwapExecuted == false) then execute_Portal_Swaps (game, addNewOrder); end --swap portals only if they haven't been swapped already; ideally they should already have been 
+	--swapped when the custom order "Portals|Portal swap" was processed during ReceiveGold phase; but if for some reason they haven't been swapped yet, do it here
+	--it's better to do it during ReceiveGold phase b/c that is processed as a regular turn phase order, and all mods will get the proper game state upon completion of the order, as opposed
+	--to processing here, where mods will receive a stale game state and SUs like Recruiters and Workers will apply their abilities to the pre-Portal swap territories (which may not even
+	--belong to the owner of the Recruiters/Workers anymore, and those SUs won't be present on those territories at the time the effects trigger)
+
 	--DESIRED FUTURE STATE:
 		--nothing happens here anymore
 		--Portal Swaps are executed in ReceiveCards phase, triggered by a custom order added in Server_AdvanceTurn_Start
@@ -7,6 +12,9 @@ function Server_AdvanceTurn_End (game, addNewOrder)
 end
 
 function Server_AdvanceTurn_Start (game, addNewOrder)
+	boolPortalSwapExecuted = false; --track whether portals have been swapped this turn or not; this is used to ensure they're only swapped once, ideally when the custom order "Portals|Portal swap"
+	--is processed during ReceiveGold phase; but if for some reason that is not received, process it in Server_AdvanceTurn_End instead to ensure that portals are swapped during the turn
+
 	-- local intSomePlayerID = (function(t) for k,_ in pairs(t) do return k end end)(game.ServerGame.Game.Players); --get playerID of 1st player in the list game.ServerGame.Game.Players; this is needed b/c GameOrderCustom required the playerID of an actual player in the game and doesn't accept 0 or WL.PlayerID.Neutral
 	-- addNewOrder (WL.GameOrderCustom.Create (intSomePlayerID, "Portals|Swap Prep", "Portals|Swap Prep", {}, WL.TurnPhase.SanctionCards, nil, nil)); --add order to invoke Portal Swaps (instead of processing in Server_AdvanceTurn_End)
 
@@ -15,6 +23,14 @@ function Server_AdvanceTurn_Start (game, addNewOrder)
 end
 
 function Server_AdvanceTurn_Order (game, order, orderResult, skipThisOrder, addNewOrder)
+	if (order.proxyType=='GameOrderCustom' and order.Payload == "Portals|Portal swap") then
+		if (boolPortalSwapExecuted == false) then execute_Portal_Swaps (game, addNewOrder); end --swap portals only if they haven't been swapped already; every human player entering
+		--turns normally from client and hitting Commit (not booting, surrendering, etc) will have this order added to their order list, but only process the Portal swaps once, not once
+		--for every iteration (every human player)
+
+		-- skipThisOrder (WL.ModOrderControl.SkipAndSupressSkippedMessage);
+	end
+
 	-- if (order.proxyType=='GameOrderCustom' and order.Payload == "Portals|Swap Prep") then
 	-- 	local intSomePlayerID = (function(t) for k,_ in pairs(t) do return k end end)(game.ServerGame.Game.Players); --get playerID of 1st player in the list game.ServerGame.Game.Players; this is needed b/c GameOrderCustom required the playerID of an actual player in the game and doesn't accept 0 or WL.PlayerID.Neutral
 	-- 	addNewOrder (WL.GameOrderCustom.Create (intSomePlayerID, "Portals|Swap", "Portals|Swap", {}, WL.TurnPhase.ReceiveGold, nil, nil)); --add order to invoke Portal Swaps (instead of processing in Server_AdvanceTurn_End)
@@ -27,6 +43,9 @@ function Server_AdvanceTurn_Order (game, order, orderResult, skipThisOrder, addN
 end
 
 function execute_Portal_Swaps (game, addNewOrder)
+	if (boolPortalSwapExecuted == true) then return; end --if portals have already swapped this turn, don't swap them again even if this is called twice for some reason (but it shouldn't be)
+
+	boolPortalSwapExecuted = true; --set to true so it only swap portals once per turn
 	local intNumPortals = Mod.Settings.NumPortals;
 	TerritoryModifications = {}; --[1] = array of terrMods for all portal terrs, stores the army swaps & up to 4 SU swaps for each terr; all these to be submitted in 1 order as 'Portal swaps'; [2,3,4...] = additional SUs
 	Annotations = {}; --annotations corresponding to TerritoryModifications
