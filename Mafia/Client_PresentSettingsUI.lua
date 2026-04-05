d6af82b0c1bc8e8255eb56465a8b23e0c1c92df8
krinid <krinid@gmail.com>
2026-04-05 02:50:22 -0400

    Mafia minor UI tweak

--- Mafia/Client_PresentSettingsUI.lua
+++ Mafia/Client_PresentSettingsUI.lua
@@ -1,4 +1,4 @@
 function Client_PresentSettingsUI(rootParent)
-	UI.CreateLabel (rootParent).SetText ("Starting turn for eliminations: " ..tostring (Mod.Settings.EliminationStartTurn).. "  (first turn a player will be eliminated)");
+	UI.CreateLabel (rootParent).SetText ("\nStarting turn for eliminations: " ..tostring (Mod.Settings.EliminationStartTurn).. "  (first turn a player will be eliminated)");
 	UI.CreateLabel (rootParent).SetText ("Frequency for eliminations: " ..tostring (Mod.Settings.EliminationTurnFrequency).. "  (# of turns before each additional player is eliminated)");
 end
