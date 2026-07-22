fb99377e0b1f7e7e21ff1d06dee61fd311f5fbd1
krinid <krinid@gmail.com>
2026-07-22 13:50:25 -0400

    Poison

--- Poison/Client_PresentSettingsUI.lua
+++ Poison/Client_PresentSettingsUI.lua
@@ -25,7 +25,7 @@ function Client_PresentSettingsUI (rootParent)
 	else UI.CreateLabel (UImain).SetText ("  • Poison effects will only impact units directly resulting from playing of Poison cards");
 	end
 
-	UI.CreateLabel (UImain).SetText ("\n# cities destroyed by Poison: ".. Mod.Settings.NumCitiesDestroyedByPoison);
+	UI.CreateLabel (UImain).SetText ("\n# cities destroyed by Poison: ".. (Mod.Settings.NumCitiesDestroyedByPoison ~= nil and tostring (Mod.Settings.NumCitiesDestroyedByPoison or 0)));
 	if (Mod.Settings.NumCitiesDestroyedByPoison > 0) then UI.CreateLabel (UImain).SetText ("Cities are destroyed: " .. tostring (Mod.Settings.CitiesAreDestroyedEachTurn and "Each turn Poison is active" or "On first Poison impact turn only")); end
 
 	UI.CreateLabel (UImain).SetText("\nNumber of pieces to divide the card into: " .. Mod.Settings.PoisonPiecesNeeded);
